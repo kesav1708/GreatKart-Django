6c83c8083416d4066577214917674896001311a4
Your Name <you@example.com>
2023-12-27 20:27:04 +0530

    Fixed issues

--- greatkart/views.py
+++ greatkart/views.py
@@ -7,6 +7,7 @@ def home(request):
   products=Product.objects.all().filter(is_available=True).order_by('-created_date')
 
    ## Getting Reviews for Product
+  reviews = None
   for product in products:
     reviews = ReviewRating.objects.filter(product_id=product.id,status=True)
 
