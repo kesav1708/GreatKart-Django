9cec8355acc257874ac42dd5a2d8f826c0ee4714
Your Name <you@example.com>
2024-01-02 01:33:22 +0530

    Deployment

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-&$5utis5jtg3k8=4tdxy7$2_-!6$wl)b*yh*tb-lks!0#wugg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
